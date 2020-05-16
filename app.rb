46c60431512c6a23984705c00d7888a7e2634a38
Dobro <matematikaigor@gmail.com>
2020-05-16 20:12:26 +0300

    finished get route

--- app.rb
+++ app.rb
@@ -4,45 +4,74 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 require 'json'
-require_relative 'functions.rb'
+require 'securerandom'
+# require_relative 'functions'
 
 set :bind, '0.0.0.0'
 set :port, 1488
 
-class MyApp < Sinatra::Base
 
-  before do
-    content_type 'application/json'
-    headers "Content-Type" => "application/json"
-  end
+before do
+  content_type 'application/json'
+  headers "Content-Type" => "application/json"
+  @connection = PG.connect :dbname => 'postgres', :user => 'postgres', :password => '676767'
+end
 
-  get '/pass/:guid' do |id|
+after do
+  @connection&.close
+end
 
-    begin
-      connection = PG.connect :dbname => 'postgres', :user => 'postgres', :password => '676767'
+get '/pass/:guid' do |id|
 
-      sql_select = connection.exec 'SELECT * FROM public."Passes";'
+  begin
+    passes = @connection.exec('SELECT * FROM public."Passes";').to_a
 
+    ids = passes.map { |item| item['GUID'] }
+    halt 404, 'NOT FOUND' unless ids.include?(id)
 
-      sql_select.to_a.each do |item|
-        # p '!!!!!!!!!!!!!!!' if item['GUID'] == id & is_valid?(item)
-        p @pass = item if item['GUID'] == id
-        time = Time.now
+    if ids.include?(id)
+      passes.each do |item|
+        halt 410, 'GONE' if item['GUID'] == id && !is_valid?(item)
+        if item['GUID'] == id && is_valid?(item)
+          @valid_pass = item
+          break
+        end
       end
-
-      body "#{@pass.to_json}, #{Time.now}"
-
-    rescue PG::Error => e
-      val_error = e.message
-
-    ensure
-      connection&.close
     end
+
+    body "#{@valid_pass.to_json}"
+
+  rescue PG::Error => e
+    e.message
   end
+end
 
-# post '/pass' do
-#   guid = generate_id
-#   p JSON.parse(request.body.read)
-# end
+get '/pass1' do
+  [200, {'zalupa' => '1'}, "Hello from Sinatra!"]
+end
 
+post "/post" do
+  p x = request.body.read
+  [200, x, "Hello from Sinatra!"]
+end
+
+
+def generate_id
+  hex = SecureRandom.hex.upcase
+  indexes = [8, 13, 18, 23]
+  indexes.each do |index|
+    hex.insert(index, '-')
+  end
+  hex
+end
+
+def is_valid?(pass)
+  year_now = Time.now.year.to_i
+  month_now = Time.now.month.to_i
+  day_now = Time.now.day.to_i
+
+  year_now.between?(pass['DateFrom'][0, 4].to_i, pass['DateTo'][0, 4].to_i)
+  month_now.between?(pass['DateFrom'][5, 2].to_i, pass['DateTo'][5, 2].to_i)
+  day_now >= pass['DateFrom'][8, 2].to_i if month_now == pass['DateFrom'][5, 2].to_i
+  day_now <= pass['DateTo'][8, 2].to_i if month_now == pass['DateTo'][5, 2].to_i
 end
